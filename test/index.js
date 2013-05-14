0812ace369d21a9d3640a13f882e1abdd47179a8
Lance Pollard <lancejpollard@gmail.com>
2013-05-13 19:06:59 -0700

    rm old tests

--- test/index.js
+++ test/index.js
@@ -125,45 +125,4 @@ describe('scope', function(){
   });
 
   it('should set `maxInstances` or something on `root` scope');
-
-  /*it('should create a new child scope', function(){
-    var child = scope('child');
-    var ctx = scope('hello')
-      .child('chx', child);
-
-    console.log(ctx);
-    assert(ctx.children['chx'] instanceof Scope);
-  });
-
-  it('should create multiple children scopes.', function(){
-    var ctx = scope('ctx')
-      .child('one')
-      .child('two')
-      .child('three');
-
-    assert(ctx.children.length === 3);
-  });
-
-  it('should find the key in the current scope.', function(){
-    var ctx = scope('ctx');
-
-    ctx.data = {
-      user: {
-        username: 'DropDrop'
-      }
-    };
-
-    assert(ctx.find('user').username === 'DropDrop');
-  });
-
-  it('should find the key in the parent scope.', function(){
-    var parent = scope('parent')
-      .child('ctx');
-
-    parent.data = {
-      user: 'Hello'
-    };
-
-    assert(scope('ctx').find('user') === 'Hello');
-  });*/
 });
